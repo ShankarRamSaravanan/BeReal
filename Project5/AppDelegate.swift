9f0f22126a12d4ac42d99a2fa54f1983e215a542
ShankarRam Saravanan <sankarams18@gmail.com>
2023-03-27 20:27:30 -0400

    First push

---
+++ Project5/AppDelegate.swift
@@ -0,0 +1,143 @@
+//
+//  AppDelegate.swift
+//  Project5
+//
+//  Created by SHANK on 3/27/23.
+//
+
+import UIKit
+import CoreData
+import ParseSwift
+
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        ParseSwift.initialize(applicationId: "zwtu7Cfq1j0mXxf6SV8TIiHoHIl3qv2D6DCRKcSB",
+                              clientKey: "obu7ZO8j7iDefDxDqBvf2o84tiTLhuVubXo8rAq0",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
+        // Override point for customization after application launch.
+        
+        var score = GameScore()
+        score.playerName = "Shankarram"
+        score.points = 18
+
+        // Save to your server asynchronously (preferred way) - Performs work on background queue and returns to specified callbackQueue.
+        // If no callbackQueue is specified it returns to main queue.
+        score.save { result in
+            switch result {
+            case .success(let savedScore):
+                print("✅ Parse Object SAVED!: Player: \(String(describing: savedScore.playerName)), Score: \(String(describing: savedScore.points))")
+            case .failure(let error):
+                assertionFailure("Error saving: \(error)")
+            }
+        }
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "Project5")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
+
+
+struct GameScore: ParseObject {
+    // These are required by ParseObject
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    // Your own custom properties.
+    // All custom properties must be optional.
+    var playerName: String?
+    var points: Int?
+}
+
+// Sample Usage
+//
+// var score = GameScore()
+// score.playerName = "Kingsley"
+// score.points = 13
+
+// OR Implement a custom initializer (OPTIONAL i.e. NOT REQUIRED)
+// It's recommended to place custom initializers in an extension
+// to preserve the memberwise initializer.
+extension GameScore {
+
+    // Use the init to set your custom properties
+    // NOTE: Properties in custom init are NOT required to be optional
+    init(playerName: String, points: Int) {
+        self.playerName = playerName
+        self.points = points
+    }
+}
+
+// Sample Usage
+//
+// let score = GameScore(playerName: "Kingsley", points: 13)
+
+// https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L33
+
+
+
